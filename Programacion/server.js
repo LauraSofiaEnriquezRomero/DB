9da699a59a4b7d9385afec433998b5c71be50c23
Arthurx7 <andres.arturo@uao.edu.co>
2023-05-20 22:27:58 -0500

    Gran avance de progrmacion

--- Programacion/server.js
+++ Programacion/server.js
@@ -3,26 +3,28 @@ const app = express();
 const db = require('./db'); // Ruta correcta al archivo de conexión a la base de datos
 const port = process.env.PORT || 5500; // Usa el puerto especificado o utiliza el puerto 3000 por defecto
 
+const path = require('path')
 
-// Configuración de rutas y lógica del servidor
-
-// Ejemplo de ruta que realiza una consulta a la base de datos
 app.get('/', (req, res) => {
-  db.query('SELECT * FROM empleado LIMIT 10;', (error, result) => {
-    if (error) throw error;
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+//Consulta 
 
-    const processedResults = results.map(item => ({
-      id: item.id,
-      nombre: item.nombre,
-    }));
-
-    res.sendFile(__dirname + '/index.html');
-
+app.get('/consulta', (req, res) => {
+  // Realiza la consulta a la base de datos utilizando el módulo db.js
+  db.query('SELECT * FROM recursos_humanos LIMIT 10;', (error, results) => {
+    if (error) {
+      console.error('Error al realizar la consulta:', error);
+      res.status(500).json({ error: 'Error al realizar la consulta' });
+    } else {
+      // Devuelve los resultados como respuesta JSON
+      res.json(results);
+    }
   });
-
 });
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
+
 
